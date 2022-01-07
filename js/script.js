1ba9c03b30004938cd73ec01a40324dec17e08ec
tallner <ctallner@hotmail.com>
2022-01-07 14:36:02 +0100

    basic styling added

--- js/script.js
+++ js/script.js
@@ -43,6 +43,7 @@ for(let i=0;i<=maxIteration;i++){
 const maxIteration = 5;
 const minHue = 150;
 const maxHue = 200;
+const fontcolor = 'hsl(230, 75%, 75%)';
 for(let i=1;i<=maxIteration;i++){
     const h3 = document.createElement('h3');
     const hue = minHue + (i*((maxHue-minHue)/maxIteration));
@@ -52,6 +53,7 @@ for(let i=1;i<=maxIteration;i++){
     h3.style.fontSize = `${i*10}px`;
     h3.style.textAlign = 'center';
     h3.style.backgroundColor = `hsl(${hue}, 75%, 75%)`;
+    h3.style.color = fontcolor;
     
     document.body.append(h3);
 }
@@ -65,21 +67,39 @@ const numbers = ['ett','två','tre','fyra','fem','sex','sju','åtta','nio','tio'
 
 //outer div with black border
 const outerDiv = document.createElement('div');
-outerDiv.style.border = 'solid black 1px';
+outerDiv.style.border = `solid black 1px`;
+outerDiv.style.display = 'flex';
+outerDiv.style.flexFlow ='row nowrap';
+outerDiv.style.justifyContent = 'center';
+//    align-items: flex-start;
+
 document.body.append(outerDiv);
 for(let i=1;i<=outerIteration;i++){
 
-    //inner divs
+    //common style inner divs
     const innerDiv = document.createElement('div');
-    innerDiv.style.border = 'solid red 1px';
+    innerDiv.style.border = `solid ${fontcolor} 20px`;
+    innerDiv.style.margin = '50px';
+    innerDiv.style.width = '50px';
+    innerDiv.style.padding = '20px';
+    
     outerDiv.append(innerDiv);
 
-    //add numbers to inner divs
+    //add numbers to inner divs and adjust text align
     for(let j=0;j<=innerIteration;j++){
         const p = document.createElement('p');
-        if (i===1) {p.innerText = j;}
-        else if (i===2) {p.innerText = innerIteration-j;}
-        else if (i===3) {p.innerText = numbers[j];}
+        if (i===1) {
+            p.innerText = j;
+            innerDiv.style.textAlign = 'left';
+        }
+        else if (i===2) {
+            p.innerText = innerIteration-j;
+            innerDiv.style.textAlign = 'center';
+        }
+        else if (i===3) {
+            p.innerText = numbers[j];
+            innerDiv.style.textAlign = 'right';
+        }
         
         innerDiv.appendChild(p);
     }
