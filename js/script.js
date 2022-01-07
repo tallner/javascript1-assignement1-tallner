a4fe48c8039c7e8ea96f51ad52b101b909358f08
tallner <ctallner@hotmail.com>
2022-01-07 15:12:49 +0100

    colors to text and divs added

--- js/script.js
+++ js/script.js
@@ -1,44 +1,3 @@
-//Flexbox
-
-
-//dagens tester
-/** * /
-for (let index = 0; index <= 10; index++) {
-    const h1 = document.createElement('h3');
-
-    h1.innerText = index;
-
-    h1.append;
-
-    document.body.append(h1);
-
-    if (index < 6){
-        h1.style.backgroundColor='blue';
-    } else if (index===7){
-        h1.style.backgroundColor='red';
-    } else {
-        h1.style.backgroundColor='grey';
-    }
-
-    if (index%2) {
-        h1.style.backgroundColor='pink';
-    }
-}
-
-/** /
-const maxIteration = 50;
-const minHue = 180;
-const maxHue = 300;
-for(let i=0;i<=maxIteration;i++){
-    const h3 = document.createElement('h3');
-    const hue = minHue + (i*((maxHue-minHue)/maxIteration));
-    const texten = hue;
-
-    h3.innerText = `Hue: ${texten}`;
-    h3.style.backgroundColor = `hsl(${hue}, 75%, 75%)`;
-    
-    document.body.append(h3);
-}
 /** */
 const maxIteration = 5;
 const minHue = 150;
@@ -63,7 +22,17 @@ for(let i=1;i<=maxIteration;i++){
 //create the number divs
 const outerIteration = 3;
 const innerIteration = 9;
+
 const numbers = ['ett','två','tre','fyra','fem','sex','sju','åtta','nio','tio'];
+const div1Color = ['black','white','black','white',fontcolor,'white','black','white','black','white'];
+const div1TextColor = ['white','black','white','black','white','black','white','black','white','black'];
+
+const div2Color = ['white',fontcolor,'white','black','white','black','white','black','white','black'];
+const div2TextColor = ['black','white','black','white','black','white',fontcolor,'white','black','white'];
+
+const div3Color = ['black','white','black','white','black',fontcolor,'black','white','black','white'];
+const div3TextColor = ['white','black','white','black','white','black','black','black','white','black'];
+
 
 //outer div with black border
 const outerDiv = document.createElement('div');
@@ -71,6 +40,8 @@ outerDiv.style.border = `solid black 1px`;
 outerDiv.style.display = 'flex';
 outerDiv.style.flexFlow ='row nowrap';
 outerDiv.style.justifyContent = 'center';
+outerDiv.style.margin = '0px';
+outerDiv.style.padding = '0px';
 //    align-items: flex-start;
 
 document.body.append(outerDiv);
@@ -81,25 +52,34 @@ for(let i=1;i<=outerIteration;i++){
     innerDiv.style.border = `solid ${fontcolor} 20px`;
     innerDiv.style.margin = '50px';
     innerDiv.style.width = '50px';
-    innerDiv.style.padding = '20px';
+    innerDiv.style.padding = '0px';
     
     outerDiv.append(innerDiv);
 
     //add numbers to inner divs and adjust text align
     for(let j=0;j<=innerIteration;j++){
         const p = document.createElement('p');
+        p.style.margin = '0px';
+        p.style.padding = '0px';
         if (i===1) {
             p.innerText = j;
+            p.style.backgroundColor = div1Color[j];
+            p.style.color = div1TextColor[j];
             innerDiv.style.textAlign = 'left';
         }
         else if (i===2) {
             p.innerText = innerIteration-j;
+            p.style.backgroundColor = div2Color[j];
+            p.style.color = div2TextColor[j];
             innerDiv.style.textAlign = 'center';
         }
         else if (i===3) {
             p.innerText = numbers[j];
+            p.style.backgroundColor = div3Color[j];
+            p.style.color = div3TextColor[j];
             innerDiv.style.textAlign = 'right';
         }
+
         
         innerDiv.appendChild(p);
     }
